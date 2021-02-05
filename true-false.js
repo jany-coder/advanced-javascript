9833be8d8b2c59912bfe3a3927cbbb585c4f9a50
jany-coder <jany_ete@yahoo.com>
2021-02-06 01:47:51 +0600

    true vs false

---
+++ true-false.js
@@ -0,0 +1,18 @@
+//false list:
+// 0
+// ""
+// undefined
+// null
+// NaN
+
+
+
+//const name  = '';
+let name;
+console.log(name);
+
+if (name) {
+    console.log('condition is true');
+} else {
+    console.log('condition is false');
+}
